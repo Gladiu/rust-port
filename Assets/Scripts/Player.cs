ffeae4ac54b9d0eb192495856bbe894395596d17
JakubSzukala <burski09@outlook.com>
2022-11-01 18:19:53 +0100

    Add comparers for keys status and combo class

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -53,3 +53,65 @@ public class InputMonitorSystem
 
     public static bool IsSnapshotEmpty(Dictionary<string, bool> token) => !token.ContainsValue(true);
 }
+
+public class SnapshotComparer : IEqualityComparer<Dictionary<string, bool>>
+{
+    // Dictionary is reference type - its bits cannot be just checked
+    // https://stackoverflow.com/questions/3804367/testing-for-equality-between-dictionaries-in-c-sharp
+    // https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.equalitycomparer-1?view=net-6.0
+    public bool Equals(Dictionary<string, bool> s1, Dictionary<string, bool> s2)
+    {
+        if(s1 == s2) return true;
+        if((s1 == null) || (s2 == null)) return false;
+        if(s1.Count != s2.Count) return false;
+
+        foreach(var kvp in s1)
+        {
+            bool val;
+            if(!s2.TryGetValue(kvp.Key, out val)) return false;
+            if(!(kvp.Value == val)) return false;
+        }
+        return true;
+    }
+
+    public int GetHashCode(Dictionary<string, bool> s)
+    {
+        // TODO: Is correct? Maybe not even necessary
+        return s.GetHashCode();
+    }
+}
+
+public class Combo
+{
+    public List<Dictionary<string, bool>> combo_sequences { get; set; }
+}
+
+public class ComboComparer : IEqualityComparer<Combo>
+{
+    // Must be overriden over object.Equals (strange...)
+    public bool Equals(Combo c1, Combo c2)
+    {
+        if(c1 == c2) return true;
+        if((c1 == null) || (c2 == null)) return false;
+        if(c1.combo_sequences.Count != c2.combo_sequences.Count) return false;
+
+        var snapshot_comparer = new SnapshotComparer();
+
+        for(int i = 0; i < c1.combo_sequences.Count; i++)
+        {
+            // Order matters when performing combo
+            if(!snapshot_comparer.Equals(
+                c1.combo_sequences[i], c2.combo_sequences[i]))
+                return false;
+        }
+        return true;
+    }
+
+    public int GetHashCode(Combo c)
+    {
+        return c.GetHashCode();
+    }
+}
+
+
+
