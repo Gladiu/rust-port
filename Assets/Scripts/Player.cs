4b284cd2da23fdd45454d032da4b064c9e73fe64
JakubSzukala <burski09@outlook.com>
2022-11-05 19:41:36 +0100

    Add input monitor with current state saver

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using System;
 
 [RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour
 {
     public float speed = 3.0F;
     public float rotateSpeed = 3.0F;
+    public InputMonitorSystem inputMonitorSystem = new InputMonitorSystem(Input.GetButton, 2000,
+        "ComboUp",
+        "ComboDown",
+        "ComboLeft",
+        "ComboRight"
+    );
 
     void Update()
     {
@@ -18,5 +25,74 @@ public class Player : MonoBehaviour
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         float curSpeed = speed * Input.GetAxis("Vertical");
         controller.SimpleMove(forward * curSpeed);
+
+        inputMonitorSystem.CaptureInputSnapshot();
+        if(!inputMonitorSystem.currentSnapshotState.IsSnapshotEmpty())
+        {
+            Debug.Log(inputMonitorSystem.currentSnapshotState);
+        }
+    }
+}
+
+public class InputMonitorSystem
+{
+    private string[] monitoredKeys { set; get; }
+    private Func<string, bool> monitorFunction { set; get; }
+    private float monitorWindow { set; get; }
+    public InputSnapshot currentSnapshotState { set; get; } // State
+
+    public InputMonitorSystem(Func<string, bool> monitorFunction, float monitorWindow, params string[] monitoredKeysNames)
+    {
+        this.monitorFunction = monitorFunction;
+        this.monitoredKeys = monitoredKeysNames;
+        this.monitorWindow = monitorWindow;
+
+        var dict = new Dictionary<string, bool>();
+        foreach(string key in monitoredKeysNames)
+        {
+            dict[key] = false;
+        }
+        this.currentSnapshotState = new InputSnapshot(dict);
+    }
+
+    public void CaptureInputSnapshot()
+    {
+        foreach(string key in this.monitoredKeys)
+        {
+            currentSnapshotState.snapshotDict[key] = this.monitorFunction(key);
+        }
+    }
+}
+
+// Simple class wrapping specific dictionary and exposing necessary features
+public class InputSnapshot
+{
+    public Dictionary<string, bool> snapshotDict { set; get; }
+    public InputSnapshot(Dictionary<string, bool> snapshotDict)
+    {
+        this.snapshotDict = snapshotDict;
+    }
+
+    public InputSnapshot(InputSnapshot snapshot)
+    {
+        this.snapshotDict = snapshot.snapshotDict;
+    }
+
+    // Perform OR operation on boolean values representing key presses
+    public void UpdateSnapshot()
+    {
+
+    }
+
+    public bool IsSnapshotEmpty() => !this.snapshotDict.ContainsValue(true);
+
+    public override string ToString()
+    {
+        string toPrint = "";
+        foreach(var kvp in this.snapshotDict)
+        {
+            toPrint += String.Format("{0} - {1}\n", kvp.Key, kvp.Value);
+        }
+        return toPrint;
     }
 }
