5324bee10ac13a2503a346f831865b5c488ce217
JakubSzukala <burski09@outlook.com>
2022-11-19 18:38:57 +0100

    Add documentation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -39,6 +39,9 @@ public class Player : MonoBehaviour
     }
 }
 
+/// <summary>
+/// Class for registering key presses during specified time window.
+/// </summary>
 public class InputMonitorSystem
 {
     private readonly string[] monitoredKeys;
@@ -48,6 +51,12 @@ public class InputMonitorSystem
     private readonly Dictionary<string, int> snapshotKeyIndexMap;
     public InputSnapshot snapshotState { set; get; } // State
 
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="monitorFunction"></param> - Unity function that will be used to get the state of the key (e.g. InputGetButton).
+    /// <param name="timeout"></param> - time window in which input will be registered. During this time window, all input will be treated as a one state snapshot.
+    /// <param name="monitoredKeysNames"></param> - Array of key names to be monitored.
     public InputMonitorSystem(Func<string, bool> monitorFunction, float timeout, params string[] monitoredKeysNames)
     {
         this.monitorFunction = monitorFunction;
@@ -65,6 +74,11 @@ public class InputMonitorSystem
         this.timer = 0;
     }
 
+    /// <summary>
+    /// Method that will use <see cref="InputMonitorSystem.monitorFunction"/> combined with <see cref="InputMonitorSystem.monitoredKeys"/>
+    /// to record key presses state in the current frame. This method will modify <see cref="InputMonitorSystem.snapshotState"/> with captured
+    /// keys state in the current frame. Values will be OR'ed.
+    /// </summary>
     public void CaptureInputSnapshot()
     {
         var currentFrameSnapshotState = new InputSnapshot(this.snapshotKeyIndexMap);
@@ -79,6 +93,11 @@ public class InputMonitorSystem
         this.snapshotState.UpdateSnapshot(currentFrameSnapshotState);
     }
 
+    /// <summary>
+    /// Method that will return <see cref="InputSnapshot"/> object containing recorded key presses from the last time window.
+    /// It will also reset <see cref="InputMonitorSystem.snapshotState"/>.
+    /// </summary>
+    /// <returns>Object of type <see cref="InputSnapshot"/> containing key presses from the last time window.</returns>
     public InputSnapshot GetSnapshotState()
     {
         var temp = this.snapshotState;
@@ -86,52 +105,105 @@ public class InputMonitorSystem
         return temp;
     }
 
+    /// <summary>
+    /// Check if timer for current time window is running.
+    /// </summary>
+    /// <returns> true if timer is running and false if it is not. </returns>
     public bool TimerOn() => this.timer > 0;
 
+    /// <summary>
+    /// Check if timer times out.
+    /// </summary>
+    /// <returns> true if timer timed out or false if it did not. </returns>
     public bool TimerTimeout() => this.timer > this.timeout;
 
+    /// <summary>
+    /// Update the timer with current delta time of a frame, use <see cref="Input.GetButton(string)"/> or similar to provide delta time.
+    /// </summary>
+    /// <param name="dt"></param> - delta time in seconds.
     public void TimerUpdate(float dt)  => this.timer += dt;
 
+    /// <summary>
+    /// Reset <see cref="InputSnapshot.timer"/> to 0. Call it after /// <see cref="InputSnapshot.TimerTimeout"/> returns true.
+    /// </summary>
     public void TimerReset() => this.timer = 0;
 }
 
-// Simple class wrapping specific dictionary and exposing necessary features
+/// <summary>
+/// Class mostly for storing the pressed keys input state. <see cref="InputSnapshot.snapshotState"/> has a type <see cref="SnapshotState"/> that is just
+/// an alias for int. Each key state is stored as a bit inside that int (1 for key pressed and 0 for not pressed). In
+/// <see cref="InputSnapshot.snapshotKeyIndexesMap"/> is stored mapping, from keys names to indexes of corresponding
+/// state bits in <see cref="InputSnapshot.snapshotState"/>. This way, we can quickly and efficiently manipulate bits but also use efficient
+/// "hash map like" access.
+/// </summary>
 public class InputSnapshot
 {
     private readonly Dictionary<string, int> snapshotKeyIndexesMap; // Map name of a key to index of a bit in snapshotState
     private SnapshotState snapshotState; // int in which each bit is state of one of keys, mapped by snapshotKeyIndexesMap
 
+    /// <summary>
+    /// Constructor for <see cref="InputSnapshot"/> class. Initializes state to 0 (no key presses) and loads mapping from string key names to state bit indexes.
+    /// </summary>
+    /// <param name="snapshotKeyIndexMap"></param> - mapping from string key name to bit index in <see cref="InputSnapshot.snapshotState"/>.
     public InputSnapshot(Dictionary<string, int> snapshotKeyIndexMap)
     {
         this.snapshotKeyIndexesMap = snapshotKeyIndexMap;
         this.snapshotState = 0;
     }
 
+    /// <summary>
+    /// Sets state bit for specified key name using <see cref="InputSnapshot.snapshotKeyIndexesMap"/>.
+    /// </summary>
+    /// <param name="keyName"></param> - key to <see cref="InputSnapshot.snapshotKeyIndexesMap"/> for which state bit will be set.
     public void SetKeyBitByName(string keyName)
     {
         int index = this.snapshotKeyIndexesMap[keyName];
         SetKeyBitByIndex(index);
     }
 
+    /// <summary>
+    /// Clears state bit for specified key name using <see cref="InputSnapshot.snapshotKeyIndexesMap"/>.
+    /// </summary>
+    /// <param name="keyName"></param> - key to <see cref="InputSnapshot.snapshotKeyIndexesMap"/> for which state bit will be cleared.
     public void ClearKeyBitByName(string keyName)
     {
         int index = this.snapshotKeyIndexesMap[keyName];
         ClearKeyBitByIndex(index);
     }
 
+    /// <summary>
+    /// Gets state bit for specified key name using <see cref="InputSnapshot.snapshotKeyIndexesMap"/>.
+    /// </summary>
+    /// <param name="keyName"></param> - key to <see cref="InputSnapshot.snapshotKeyIndexesMap"/> for which state will be returned.
     public int GetBitByName(string keyName)
     {
         int index = this.snapshotKeyIndexesMap[keyName];
         return GetBitByIndex(index);
     }
 
+    /// <summary>
+    /// Sets a state bit given the index of it.
+    /// </summary>
+    /// <param name="idx"></param> - index of bit to set.
     private void SetKeyBitByIndex(int idx) => this.snapshotState |= (1 << idx);
 
+    /// <summary>
+    /// Clears a state bit given the index of it.
+    /// </summary>
+    /// <param name="idx"></param> - index of bit to set.
     private void ClearKeyBitByIndex(int idx) => this.snapshotState &= ~(1 << idx);
 
+    /// <summary>
+    /// Returns a state bit given the index of it.
+    /// </summary>
+    /// <param name="idx"></param> - index of a state bit to return.
+    /// <returns></returns> - state bit value 0 or 1.
     private int GetBitByIndex(int idx) => (this.snapshotState >> idx) & 1;
 
-    // Perform OR operation
+    /// <summary>
+    /// Update this <see cref="InputSnapshot"/> object's state with state values from argument. Values are updated with OR operation.
+    /// </summary>
+    /// <param name="snapshot"></param> - <see cref="InputSnapshot"/> object to update state from.
     public void UpdateSnapshot(InputSnapshot snapshot)
     {
         this.snapshotState |= snapshot.snapshotState;
@@ -142,8 +214,16 @@ public class InputSnapshot
         this.snapshotState |= snapshotState;
     }
 
+    /// <summary>
+    /// Function to determine if the the snapshot is empty (no key was pressed = no state bit is set).
+    /// </summary>
+    /// <returns> true when snapshot is empty and false when it is not. </returns>
     public bool IsSnapshotEmpty() => this.snapshotState == 0;
 
+    /// <summary>
+    /// Override of <see cref="ToString"/> function. Gives clear representation of the <see cref="InputSnapshot"/> object.
+    /// </summary>
+    /// <returns></returns>
     public override string ToString()
     {
         string toPrint = "";
